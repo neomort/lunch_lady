07a00439e90d9659161eda999414e6a9ec3e5abe
neomort <kyle@kyleadams.com>
2020-08-20 14:13:42 -0600

    wrote some pseudo code

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ the user chooses from a list of main dishes
 the user chooses 2 side dish items
 computer repeats users order
 computer totals lunch items and displays total -->
+
---
+++ menu.rb
@@ -0,0 +1,24 @@
+#{name: 'string' price:float, ingredients:['rice', 'beans']}
+
+#dish would be an instance of class
+
+# Dish - handles both main and side dishes
+
+#sideDish class and mainDish class
+
+#app.rb to hanfle logic
+
+#resturaunt class?
+
+class Dish
+  @price
+  @name
+end
+
+class Resturaunt
+
+@main_dishes [ Dish.new ('spaghetti', 23.00) Dish.new ('chicken', 44.33 )]
+@side_dishes [ Dish.new ]
+
+end
+
